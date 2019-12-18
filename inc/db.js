bb2e17f1796320c8eece9db7bbb73f7cf7202e5a
Almeida - 748 <rafael.lara@cairunet.ad.br>
2019-12-18 13:46:38 -0400

    rt

--- inc/db.js
+++ inc/db.js
@@ -1,11 +1,20 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
+/*const connection = mysql.createConnection({
     host: 'localhost',
     user: 'user',
     database: 'transporteturismo',
     password: '#Operadoryale10',
     multipleStatements: true
-});
+});*/
+
+const connection = mysql.createConnection({
+    host: 'us-cdbr-iron-east-05.cleardb.net',
+    user: 'b41627b4d1afdf',
+    database: 'heroku_2ebc4bb05bbf1d3',
+    password: '43960393',
+    multipleStatements: true
+  });
+
 
 module.exports = connection;
